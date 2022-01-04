778547fbb6f62484ef0e0ce12f3db7f202593cd1
SolarFertigation <dev@solarfertigation.com>
2022-01-04 16:31:23 +0000

    new olddevelop path

--- olddevelop/init_files/foreverStartup.sh
+++ olddevelop/init_files/foreverStartup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 5
-cd /home/pi/SF-OLD/olddevelop/
+cd /home/pi/sf-old/olddevelop/
 forever start forever.json --watchIgnore '*.log'>>/home/pi/forever_logs/output.log 2>>/home/pi/forever_logs/error.log
