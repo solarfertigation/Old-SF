4452efa939c97e7777f7cd862b4babf2109f9774
SolarFertigation <dev@solarfertigation.com>
2022-02-22 19:09:36 +0000

    new gpio

--- olddevelop/config.js
+++ olddevelop/config.js
@@ -12,19 +12,19 @@ try {
         hash: md5(hostname),
         //mqtt configuration,
         kalive: 200,
-        user: "gianluca",
+        user: "alessandro",
         clean: false,
-        pass: "Solarmq",
+        pass: "solar2021!",
         url: 'mqtt://hairdresser.cloudmqtt.com:15700',
         //gpios configuration,
         zones: [23,24,25],
-        ferts: [18,12,16],
-        // newferts: [17,27,22],
+        ferts: [17,27,22],
+        // newferts: [18,12,16],
         // mix: 18,
-        recirc: 20,
-        // ev_venturi: 13
-        water_pump: 21
-        // water_pump: 23
+        recirc: 13,
+        // old_recirc: 20
+        water_pump: 23
+        // water_pump: 21
         // ev: [19,26,12,16,20,21]
         // ava: [04,24,25,06]
     }
