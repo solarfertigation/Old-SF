2d9e4e73ddf73d118528fad5ff3197d85e7d3750
SolarFertigation <dev@solarfertigation.com>
2022-07-11 16:44:56 +0000

    new flask

--- gpio/sf-webserver/app.py
+++ gpio/sf-webserver/app.py
@@ -6,14 +6,14 @@ GPIO.setmode(GPIO.BCM)
 
 # Create a dictionary called pins to store the pin number, name, and pin state:
 pins = {
-   12 : {'name' : 'Fert 1', 'state' : GPIO.LOW},
-   16 : {'name' : 'Fert 2', 'state' : GPIO.LOW},
-   18 : {'name' : 'Fert 3', 'state' : GPIO.LOW},
+   12 : {'name' : 'Fertilizzante 1', 'state' : GPIO.LOW},
+   16 : {'name' : 'Fertilizzante 2', 'state' : GPIO.LOW},
+   18 : {'name' : 'Fertilizzante 3', 'state' : GPIO.LOW},
    20 : {'name' : 'Ricircolo', 'state' : GPIO.LOW},
    21 : {'name' : 'Pompa', 'state' : GPIO.LOW},
-   23 : {'name' : 'EV1', 'state' : GPIO.LOW},
-   24 : {'name' : 'EV2', 'state' : GPIO.LOW},
-   25 : {'name' : 'EV', 'state' : GPIO.LOW}
+   23 : {'name' : 'ElettroValvola 1', 'state' : GPIO.LOW},
+   24 : {'name' : 'Elettrovalvola 2', 'state' : GPIO.LOW},
+   25 : {'name' : 'Elettrovalvola', 'state' : GPIO.LOW}
    }
 
 # Set each pin as an output and make it low:
